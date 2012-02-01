eea7ac7c36b942f02b2a9e185d52e1bf69a77e2a
Lee Coomber <lee@lshift.net>
2012-02-01 22:32:35 +0000

    Align predicate names with existing standard

--- dot_trace.pl
+++ dot_trace.pl
@@ -21,18 +21,18 @@ prolog_trace_interception(call, Frame, _Choice, continue):-
     % Do not display calls to stop_trace, but use the call to get a reference to
     % the start node, and rename it
     prolog_frame_attribute(Frame, predicate_indicator, dot_trace:stop_trace/1),
-    reference_parent_gen(Frame, ParentReference),
+    generate_parent_node_ref(Frame, ParentReference),
     format('    ~w [label="Start"];~n', [ParentReference]).
 
 prolog_trace_interception(call, Frame, _Choice, continue):-
-    reference_gen(Frame, Reference),
+    generate_node_ref(Frame, Reference),
     prolog_frame_attribute(Frame, goal, Goal),
     format('    ~w [label="~W"];~n', [Reference, Goal,
                                       [ quoted(true),
                                         numbervars(true),
                                         portray(true) ]]),
     
-    reference_parent_gen(Frame, ParentReference),
+    generate_parent_node_ref(Frame, ParentReference),
     format('    ~w -> ~w;~n', [ParentReference, Reference]).
 
 prolog_trace_interception(exit, Frame, _Choice, continue) :-
@@ -42,18 +42,18 @@ prolog_trace_interception(exit, Frame, _Choice, continue) :-
 prolog_trace_interception(exit, Frame, _Choice, continue):-
     prolog_frame_attribute(Frame, clause, _),
     prolog_frame_attribute(Frame, goal, Goal),
-    reference_gen(Frame, Reference),
-    reference_parent_gen(Frame, ParentReference),
+    generate_node_ref(Frame, Reference),
+    generate_parent_node_ref(Frame, ParentReference),
     format('    ~w -> ~w [label="Exit: ~w"];~n', [Reference, ParentReference, Goal]).
 
 prolog_trace_interception(_Port, _Frame, _PC, continue).
 
 
-reference_gen(Frame, Reference) :-
+generate_node_ref(Frame, Reference) :-
     prolog_frame_attribute(Frame, level, Level),    
     prolog_frame_attribute(Frame, pc, PC),
     atomic_list_concat([f, Frame, l, Level, p, PC], Reference).
 
-reference_parent_gen(Frame, Reference) :-
+generate_parent_node_ref(Frame, Reference) :-
     prolog_frame_attribute(Frame, parent, Parent),
-    reference_gen(Parent, Reference).
+    generate_node_ref(Parent, Reference).
