6d23eddb4e55e2a9d85332d8c06365b1baeafe72
Lee Coomber <lee@lshift.net>
2012-02-08 21:50:38 +0000

    Simplied node references, added some placeholders for fail and redo ports, and quoted node references

--- dot_trace.pl
+++ dot_trace.pl
@@ -22,18 +22,18 @@ prolog_trace_interception(call, Frame, _Choice, continue):-
     % the start node, and rename it
     prolog_frame_attribute(Frame, predicate_indicator, dot_trace:stop_trace/1),
     generate_parent_node_ref(Frame, ParentReference),
-    format('    ~w [label="Start"];~n', [ParentReference]).
+    format('    "~w" [label="Start"];~n', [ParentReference]).
 
 prolog_trace_interception(call, Frame, _Choice, continue):-
     generate_node_ref(Frame, Reference),
     prolog_frame_attribute(Frame, goal, Goal),
-    format('    ~w [label="~W"];~n', [Reference, Goal,
+    format('    "~w" [label="~W"];~n', [Reference, Goal,
                                       [ quoted(true),
                                         numbervars(true),
                                         portray(true) ]]),
     
     generate_parent_node_ref(Frame, ParentReference),
-    format('    ~w -> ~w;~n', [ParentReference, Reference]).
+    format('    "~w" -> "~w";~n', [ParentReference, Reference]).
 
 prolog_trace_interception(exit, Frame, _Choice, continue) :-
     % Do not display exit from start_trace
@@ -44,15 +44,23 @@ prolog_trace_interception(exit, Frame, _Choice, continue):-
     prolog_frame_attribute(Frame, goal, Goal),
     generate_node_ref(Frame, Reference),
     generate_parent_node_ref(Frame, ParentReference),
-    format('    ~w -> ~w [label="Exit: ~w"];~n', [Reference, ParentReference, Goal]).
+    format('    "~w" -> "~w" [label="Exit: ~w"];~n', [Reference, ParentReference, Goal]).
+
+prolog_trace_interception(fail, Frame, _Choice, continue):-
+    format('// fail Frame: ~w~n', Frame).
+
+prolog_trace_interception(redo, Frame, _Choice, continue):-
+    format('// redo Frame: ~w~n', Frame).
 
 prolog_trace_interception(_Port, _Frame, _PC, continue).
 
 
 generate_node_ref(Frame, Reference) :-
-    prolog_frame_attribute(Frame, level, Level),    
+    % I could not tell from a quick look of the SWI-Prolog source code what a
+    % good reference would be, so using a combination of frame and PC as a starting
+    % point
     prolog_frame_attribute(Frame, pc, PC),
-    atomic_list_concat([f, Frame, l, Level, p, PC], Reference).
+    atomic_list_concat([f, Frame, p, PC], Reference).
 
 generate_parent_node_ref(Frame, Reference) :-
     prolog_frame_attribute(Frame, parent, Parent),
